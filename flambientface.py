f49a20248cb995ec36016975ad47a2ee64874a47
hugovk <hugovk@users.noreply.github.com>
2014-04-05 11:39:39 +0300

    pep8

--- flambientface.py
+++ flambientface.py
@@ -2,8 +2,9 @@
 """
 Hacked to triangulize your face
 
-This program is demonstration for face and object detection using haar-like features.
-The program finds faces in a camera image or video stream and displays a red box around them.
+This program is demonstration for face and object detection using haar-like
+features. The program finds faces in a camera image or video stream and
+displays a red box around them.
 
 Original C implementation by:  ?
 Python implementation by: Roman Stanchak, James Bowman
@@ -17,7 +18,7 @@ import shutil
 try:
     import triangulizor
 except ImportError:
-        sys.exit('Could not import Triangulizor (do `pip install triangulizor`')
+    sys.exit('Could not import Triangulizor (do `pip install triangulizor`')
 
 try:
     import Image
@@ -27,23 +28,26 @@ except ImportError:
     except ImportError:
         sys.exit('Could not import Python Imaging Library')
 
-try: import timing # Optional, http://stackoverflow.com/a/1557906/724176
-except: None
+# Optional, http://stackoverflow.com/a/1557906/724176
+try:
+    import timing
+except:
+    pass
 
 # Parameters for haar detection
 # From the API:
-# The default parameters (scale_factor=2, min_neighbors=3, flags=0) are tuned 
-# for accurate yet slow object detection. For a faster operation on real video 
-# images the settings are: 
-# scale_factor=1.2, min_neighbors=2, flags=CV_HAAR_DO_CANNY_PRUNING, 
+# The default parameters (scale_factor=2, min_neighbors=3, flags=0) are tuned
+# for accurate yet slow object detection. For a faster operation on real video
+# images the settings are:
+# scale_factor=1.2, min_neighbors=2, flags=CV_HAAR_DO_CANNY_PRUNING,
 # min_size=<minimum possible face size
 
 min_size = (20, 20)
 image_scale = 2
 haar_scale = 1.2
 min_neighbors = 2
-# haar_flags = 0 # to detect all objects
-haar_flags = cv.CV_HAAR_FIND_BIGGEST_OBJECT # to detect only one
+# haar_flags = 0  # to detect all objects
+haar_flags = cv.CV_HAAR_FIND_BIGGEST_OBJECT  # to detect only one
 
 WINDOW_NAME = "flambient"
 TEMP_DIR = "flambient_temp"
@@ -52,8 +56,11 @@ TEMP_SUFFIX = ".png"
 
 count = 0
 
+
 def opencv_to_pil(opencv_img):
-    return Image.fromstring("RGB", cv.GetSize(opencv_img), opencv_img.tostring())
+    return Image.fromstring(
+        "RGB", cv.GetSize(opencv_img), opencv_img.tostring())
+
 
 def create_temp_dir():
     if os.path.isdir(TEMP_DIR):
@@ -61,23 +68,28 @@ def create_temp_dir():
     if not os.path.isdir(TEMP_DIR):
         os.mkdir(TEMP_DIR)
 
+
 def remove_temp_dir():
     print "Deleting temp directory"
     shutil.rmtree(TEMP_DIR)
 
+
 def start_timer():
     return cv.GetTickCount()
+
 
 def stop_timer(t, message):
     t = cv.GetTickCount() - t
     print message + "\t= %gms" % (t/(cv.GetTickFrequency()*1000.))
 
+
 def detect_and_draw(img, cascade):
     global count
     # allocate temporary images
-    gray = cv.CreateImage((img.width,img.height), 8, 1)
-    small_img = cv.CreateImage((cv.Round(img.width / image_scale),
-    		       cv.Round (img.height / image_scale)), 8, 1)
+    gray = cv.CreateImage((img.width, img.height), 8, 1)
+    small_img = cv.CreateImage((
+        cv.Round(img.width / image_scale),
+        cv.Round(img.height / image_scale)), 8, 1)
 
     # convert color input image to grayscale
     cv.CvtColor(img, gray, cv.CV_BGR2GRAY)
@@ -89,20 +101,21 @@ def detect_and_draw(img, cascade):
 
     if(cascade):
         t = start_timer()
-        faces = cv.HaarDetectObjects(small_img, cascade, cv.CreateMemStorage(0),
-                                     haar_scale, min_neighbors, haar_flags, min_size)
+        faces = cv.HaarDetectObjects(
+            small_img, cascade, cv.CreateMemStorage(0),
+            haar_scale, min_neighbors, haar_flags, min_size)
         stop_timer(t, "detection time")
         if faces:
             for ((x, y, w, h), n) in faces:
                 print "Found"
                 t0 = start_timer()
-                # the input to cv.HaarDetectObjects was resized, so scale the 
+                # the input to cv.HaarDetectObjects was resized, so scale the
                 # bounding box of each face and convert it to two CvPoints
                 w = int(w * image_scale)
                 h = int(h * image_scale)
                 x = int(x * image_scale)
                 y = int(y * image_scale)
-                
+
                 cropped = cv.CreateImage((w, h), img.depth, img.nChannels)
                 src_region = cv.GetSubRect(img, (x, y, w, h))
                 cv.Copy(src_region, cropped)
@@ -115,12 +128,15 @@ def detect_and_draw(img, cascade):
 
                 # Triangulize!
                 t = start_timer()
-                pil_image = triangulizor.triangulize(pil_image, options.tile_size)
+                pil_image = triangulizor.triangulize(
+                    pil_image, options.tile_size)
                 stop_timer(t, "triangulize")
 
                 # PIL to OpenCV
                 t = start_timer()
-                cropped = cv.CreateImageHeader(pil_image.size, cv.IPL_DEPTH_8U, 3) # depth, channels
+                # Depth, channels:
+                cropped = cv.CreateImageHeader(
+                    pil_image.size, cv.IPL_DEPTH_8U, 3)
                 cv.SetData(cropped, pil_image.tostring())
                 stop_timer(t, "PIL to OpenCV")
 
@@ -131,27 +147,38 @@ def detect_and_draw(img, cascade):
                 cv.SetImageROI(img, (0, 0, img.width, img.height))
                 stop_timer(t, "paste time")
 
-                # cv.Rectangle(img, (x, y), (x + w, y + h), cv.RGB(255, 0, 0), 3, 8, 0)
+                # cv.Rectangle(
+                #    img, (x, y), (x + w, y + h), cv.RGB(255, 0, 0), 3, 8, 0)
                 stop_timer(t0, "process time")
 
     cv.ShowImage(WINDOW_NAME, img)
     if options.outfile:
         t = start_timer()
-        outfile = os.path.join(TEMP_DIR, TEMP_BASE + str(count).zfill(6) + TEMP_SUFFIX)
+        outfile = os.path.join(
+            TEMP_DIR, TEMP_BASE + str(count).zfill(6) + TEMP_SUFFIX)
         cv.SaveImage(outfile, img)
         stop_timer(t, "save time")
         count += 1
 
 if __name__ == '__main__':
 
-    parser = OptionParser(usage = "usage: %prog [options] [filename|camera_index (hint: try 1)]")
-    parser.add_option("-c", "--cascade", action="store", dest="cascade", type="str", help="Haar cascade file, default %default", default = "D:\\temp\\opencv\\data\\haarcascades\\haarcascade_frontalface_alt.xml")
-    parser.add_option("-t", "--tile-size", type=int, default=30, help='Tile size (should be divisible by 2. Use 0 to guess based on image size).') # 0 is to guess
-    parser.add_option("-o", "--outfile", help='Output filename of animated gif')
+    parser = OptionParser(
+        usage="usage: %prog [options] [filename|camera_index (hint: try 1)]")
+    parser.add_option(
+        "-c", "--cascade", action="store", dest="cascade", type="str",
+        help="Haar cascade file, default %default",
+        default="D:\\temp\\opencv\\data\\haarcascades\\"
+        "haarcascade_frontalface_alt.xml")
+    parser.add_option(
+        "-t", "--tile-size", type=int, default=30,
+        help='Tile size (should be divisible by 2. '
+        'Use 0 to guess based on image size).')
+    parser.add_option(
+        "-o", "--outfile", help='Output filename of animated gif')
     (options, args) = parser.parse_args()
 
     cascade = cv.Load(options.cascade)
-    
+
     if len(args) != 1:
         parser.print_help()
         sys.exit(1)
@@ -169,27 +196,27 @@ if __name__ == '__main__':
         frames = []
         if options.outfile:
             create_temp_dir()
-        
+
         while True:
             frame = cv.QueryFrame(capture)
             if not frame:
                 cv.WaitKey(0)
                 break
             if not frame_copy:
-                frame_copy = cv.CreateImage((frame.width,frame.height),
+                frame_copy = cv.CreateImage((frame.width, frame.height),
                                             cv.IPL_DEPTH_8U, frame.nChannels)
             if frame.origin == cv.IPL_ORIGIN_TL:
                 cv.Copy(frame, frame_copy)
             else:
                 cv.Flip(frame, frame_copy, 0)
-            
+
             detect_and_draw(frame_copy, cascade)
 
             if cv.WaitKey(10) >= 0:
                 break
-        
+
         cv.DestroyWindow(WINDOW_NAME)
-        
+
         if options.outfile:
             t = start_timer()
             print "Create animated gif"
@@ -199,7 +226,7 @@ if __name__ == '__main__':
             os.system(cmd)
             stop_timer(t, "create time")
             remove_temp_dir()
-        
+
     else:
         image = cv.LoadImage(input_name, 1)
         detect_and_draw(image, cascade)
